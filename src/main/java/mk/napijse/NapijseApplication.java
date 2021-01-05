1d0be98580071be93279268f8ddbb486151837cb
Robert Ilievski <rilievski54@gmail.com>
2021-01-05 15:52:28 +0100

    Created the project, its initial structure and added dependencies

---
+++ src/main/java/mk/napijse/NapijseApplication.java
@@ -0,0 +1,13 @@
+package mk.napijse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NapijseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NapijseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mk/napijse/NapijseApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.napijse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NapijseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
