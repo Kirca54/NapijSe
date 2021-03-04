2c9c9c3e894783b71ac0cc75dbb050db6e6810bd
Robert Ilievski <rilievski54@gmail.com>
2021-03-04 11:37:47 +0100

    Fixed delete from favourites method

--- src/main/java/mk/napijse/web/LoginController.java
+++ src/main/java/mk/napijse/web/LoginController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -22,7 +23,12 @@ public class LoginController {
     }
 
     @GetMapping
-    public String getLoginPage(Model model) {
+    public String getLoginPage(@RequestParam(required = false) String error,
+                               Model model) {
+        if(error != null && !error.isEmpty()) {
+            model.addAttribute("hasError", true);
+            model.addAttribute("error", error);
+        }
         model.addAttribute("bodyContent","login");
         return "master-template";
     }
